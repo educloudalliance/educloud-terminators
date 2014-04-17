af428e24e7407e97262e1d8748167d2ea27085df
Taru Muhonen <taru.muhonen@gmail.com>
2014-04-17 23:41:26 +0300

    Set up the frontend (twitter bootstrap and angular) with yeoman. Run bower install in your local installations.

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular
+  .module('pilvivaylaTerminatorsApp', [
+    'ngCookies',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
