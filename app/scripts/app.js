8f15b566ff41b14dd87d61b7aaec448732325de4
Taru Muhonen <taru.muhonen@gmail.com>
2014-04-18 12:57:15 +0300

    Set up angular ui.router for smarter routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,10 +4,37 @@ angular
   .module('pilvivaylaTerminatorsApp', [
     'ngCookies',
     'ngSanitize',
-    'ngRoute'
+	'ui.router'
   ])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  //http://scotch.io/tutorials/javascript/angular-routing-using-ui-router
+  $urlRouterProvider.otherwise('/home');
+	//http://www.ng-newsletter.com/posts/angular-ui-router.html
+	$stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'views/main.html',
+	     controller: 'MainCtrl'
+    })
+    .state('home.list', {
+        url: '/list',
+        templateUrl: 'views/home-list.html',
+        controller: function($scope) {
+            $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+        }
+    })
+    .state('subjectt', {
+        url: '/subject',
+        templateUrl: 'views/subject.html',
+        controller: function($scope) {
+            $scope.subject = ['Maths'];
+        }
+    })
+
+
+
+    ;
+	/*$routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
@@ -18,6 +45,6 @@ angular
       })
       .otherwise({
         redirectTo: '/'
-      });
+      });*/
     $locationProvider.html5Mode(true);
   });
